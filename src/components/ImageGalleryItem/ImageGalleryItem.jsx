c6a663f978947b4181854a876ac34d2413c4ecc6
Maksim6767 <nikmaksim20@gmail.com>
2023-01-01 20:44:57 +0200

    done task

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,13 @@
+import { ImageItem, Image } from './ImageGalleryItem.styled';
+export const ImageGallaryItem = ({
+  id,
+  imageLink,
+  imageName,
+  onImageClick,
+}) => {
+  return (
+    <ImageItem onClick={onImageClick}>
+      <Image id={id} src={imageLink} alt={imageName} />
+    </ImageItem>
+  );
+};
